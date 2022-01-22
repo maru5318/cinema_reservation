b8ade7f877a347058c4e734e75a771ec5ba56049
maru <ne201082@senshu-u.jp>
2022-01-22 19:24:03 +0000

    Heroku用の設定

--- db/seeds/production/schedules.rb
+++ db/seeds/production/schedules.rb
@@ -1,14 +1,17 @@
 starttimes = %w(9:00 12:00 15:00 18:00 21:00)
-endtimes = %w(11:00 14:00 17:00 20:00 23:00)
-1.upto(10) do |a|
-    0.upto(4) do |c|
-        Schedule.create(
-        screen_no: a,
-        theater_id: 1,
-        movie_id: rand(1..10),
-        screening_date: Date.today,
-        starttime: Time.parse("#{Date.today}-#{starttimes[c]}"),
-        endtime: Time.parse("#{Date.today}-#{endtimes[c]}")
-        )
+1.upto(3) do |i|
+    0.upto(6) do |d|
+        1.upto(10) do |a|
+            0.upto(4) do |c|
+                Schedule.create(
+                screen_no: a,
+                theater_id: i,
+                movie_id: 11-a,
+                screening_date: Time.current.since(d.days),
+                starttime: Time.zone.parse("#{starttimes[c]}"),
+                endtime: Time.zone.parse("#{starttimes[c]}")+Movie.find(11-a).screening_time*60
+                )
+            end
+        end
     end
 end
