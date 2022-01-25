808ceec216b4027ecf044a62e2e5fc92022ec81b
maru <ne201082@senshu-u.jp>
2022-01-25 14:18:18 +0000

    Heroku用の設定2

--- db/seeds/development/seats.rb
+++ db/seeds/development/seats.rb
@@ -1,6 +1,6 @@
 lines = %w(A B C D E F G H I J K L M N O)
 ends = %w(30 30 30 30 30 30 30 30 30 30 40 40 40 40 40)
-0.upto(10) do |a|
+0.upto(9) do |a|
     0.upto(14) do |b|
         Seat.create(
             seat_group: a+1,
--- db/seeds/production/seats.rb
+++ db/seeds/production/seats.rb
@@ -1,11 +1,11 @@
 lines = %w(A B C D E F G H I J K L M N O)
 ends = %w(30 30 30 30 30 30 30 30 30 30 40 40 40 40 40)
-0.upto(10) do |a|
+0.upto(9) do |a|
     0.upto(14) do |b|
         Seat.create(
             seat_group: a+1,
-            line: lines[b].to_i,
-            start_line: 0,
+            line: lines[b],
+            start_line: 1,
             last_line: ends[b].to_i,
             num: ends[b].to_i
         )
